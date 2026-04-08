76410c291f26b7873a24953567729c2f51afb397
Cristian Garcia <cristian.gz1914@gmail.com>
2026-04-08 15:11:17 -0500

    Creacion de la navbar en la vista de perfil con sus opciones y actualizacion del js para el dropdown menu

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,5 +1,22 @@
+
+
 // funcion para desplegar el menu de los aside
 function desplegarMenu(){
     const menu = document.getElementById('dropDownMenu');
     menu.classList.toggle('hidden');
 }
+
+// funcion para desplegar el dropdown menu del perfil y opciones
+function desplegarMenuPerfil(){
+    const menuPerfil = document.getElementById('dropDownMenuPerfil');
+    menuPerfil.classList.toggle('hidden')
+}
+
+document.addEventListener('click', function(e) {
+    const menuPerfil = document.getElementById('dropDownMenuPerfil');
+    const boton = document.getElementById('btnPerfil');
+
+    if (!menuPerfil.contains(e.target) && !boton.contains(e.target)) {
+        menuPerfil.classList.add('hidden');
+    }
+});
